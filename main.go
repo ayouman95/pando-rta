23806a700e7d3f93e7629e773957250d65b98732
ayouman95 <156737219+ayouman95@users.noreply.github.com>
2025-10-10 10:42:33 +0800

    update log file num

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ const (
 	TargetAPIReport  = "https://growth-rta.tiktokv-us.com/api/v1/rta/report"
 	LogFile          = "./logs/api.log" // 所有日志写入这个文件，lumberjack 负责滚动
 	MaxSize          = 1000             // 每个日志文件最大 100MB
-	MaxBackups       = 200              // 最多保留 10 个备份文件
+	MaxBackups       = 100              // 最多保留 10 个备份文件
 )
 
 // 初始化日志（每天一个文件，使用 lumberjack 滚动）
